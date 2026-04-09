c796f527006a39551a39e29af54fd61a4e787b03
gabrielnunes03 <gabriel.nunes03@unifesp.br>
2026-04-09 13:53:45 -0300

    Create 1000.C
    beecrowd problema 1000.C resposta

---
+++ 1000.C
@@ -0,0 +1,12 @@
+#include <stdio.h>
+ 
+int main() {
+    prinf("Hello World!\n!");
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+ 
+    return 0;
+}
