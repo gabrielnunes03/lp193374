1ad434be735accae10a2920f2dc25157bef9205e
gabrielnunes03 <gabriel.nunes03@unifesp.br>
2026-04-29 17:52:09 -0300

    Update 1000.C

--- 1000.C
+++ 1000.C
@@ -1,3 +1,12 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : <<<Gabriel Nunes de Abreu de Lima Moraes>>>
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1000
+Data        : 29/04/2026
+Objetivo    : Escrever Hello World.
+Aprendizado : <<<Aprende-se o básico sobre a estrutura da linguaguem C, e o comando printf>>>
+-------------------------------------------------------------------------- */
 #include <stdio.h>
  
 int main() 
