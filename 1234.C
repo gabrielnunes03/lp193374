5977aa53f6ccdd290b2a95df8786673b0222c5e0
gabrielnunes03 <gabriel.nunes03@unifesp.br>
2026-06-24 02:59:02 -0300

    Update 1234.C

--- 1234.C
+++ 1234.C
@@ -1,3 +1,12 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : <<<Gabriel Nunes de Abreu de Lima Moraes>>>
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1234
+Data        : 29/04/2026
+Objetivo    : Sentenca dancante
+Aprendizado : <<<rever strings>>>
+-------------------------------------------------------------------------- */
 #include <string.h>
 #include <stdio.h>
 #include <ctype.h>
