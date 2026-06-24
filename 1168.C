b5146c13bbd0165179e49eb014a36b33a202a24e
gabrielnunes03 <gabriel.nunes03@unifesp.br>
2026-06-23 23:40:48 -0300

    Update 1168.C

--- 1168.C
+++ 1168.C
@@ -1,3 +1,12 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : <<<Gabriel Nunes de Abreu de Lima Moraes>>>
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1168
+Data        : 23/06/2026
+Objetivo    : ajudar a achar a quantidade de leds necessários.
+Aprendizado : <<<Aprende-se sobre o comando switch>>>
+-------------------------------------------------------------------------- */
 #include <stdio.h>
 #include <string.h>
 
