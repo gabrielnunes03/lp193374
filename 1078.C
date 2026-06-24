0427645b7deef06bcb61272c0fcc3ccda3260016
gabrielnunes03 <gabriel.nunes03@unifesp.br>
2026-06-24 02:57:00 -0300

    Update 1078.C

--- 1078.C
+++ 1078.C
@@ -1,3 +1,12 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : <<<Gabriel Nunes de Abreu de Lima Moraes>>>
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1441
+Data        : 23/06/2026
+Objetivo    : Tabuada
+Aprendizado : <<<comando "for" >>>
+-------------------------------------------------------------------------- */
 #include <stdio.h>
 
 int main()
