e0f68d5f47b8c71ba3a257700463b82eedf044c7
gabrielnunes03 <gabriel.nunes03@unifesp.br>
2026-06-24 02:06:45 -0300

    Update 1169.C

--- 1169.C
+++ 1169.C
@@ -1,3 +1,12 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : <<<Gabriel Nunes de Abreu de Lima Moraes>>>
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1169
+Data        : 23/06/2026
+Objetivo    : número de quadrados a serem usados em um tabuleiro de damas e que represente a quantidade de kg de trigo correspondente
+Aprendizado : <<recursão com numeros grandes>>>
+-------------------------------------------------------------------------- */
 #include <stdio.h>
 
 int main() {
