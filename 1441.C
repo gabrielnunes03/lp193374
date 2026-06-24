b7971b8a18b78931a21db456934bb44884b20432
gabrielnunes03 <gabriel.nunes03@unifesp.br>
2026-06-24 02:52:47 -0300

    Update 1441.C

--- 1441.C
+++ 1441.C
@@ -1,3 +1,12 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : <<<Gabriel Nunes de Abreu de Lima Moraes>>>
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1441
+Data        : 23/06/2026
+Objetivo    : Sequências de Granizo
+Aprendizado : <<<recursão>>>
+-------------------------------------------------------------------------- */
 #include <stdio.h>
 
 int calcular_maior_collatz(int n, int maior_atual) {
