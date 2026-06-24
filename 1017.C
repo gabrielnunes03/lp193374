d6693f29ff73a74ab5fcbf84d1393f26b391cabd
gabrielnunes03 <gabriel.nunes03@unifesp.br>
2026-06-23 23:06:55 -0300

    Update 1017.C

--- 1017.C
+++ 1017.C
@@ -1,3 +1,12 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : <<<Gabriel Nunes de Abreu de Lima Moraes>>>
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1017
+Data        : 23/06/2026
+Objetivo    : Calcular quantos litros de combustivel é usado.
+Aprendizado : <<<Aprender a usar float, e transformar funções da matemática em código>>>
+-------------------------------------------------------------------------- */
 #include <stdio.h>
 
 float calcularLitros(int tempo, int velocidade) {
