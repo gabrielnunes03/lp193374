27cdca234e6881dabf367d3aa28b57d35ee7f0ca
gabrielnunes03 <gabriel.nunes03@unifesp.br>
2026-06-23 23:03:42 -0300

    Add header comments to 1015.C
    Added header comments with problem details and objectives.

--- 1015.C
+++ 1015.C
@@ -1,3 +1,12 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : <<<Gabriel Nunes de Abreu de Lima Moraes>>>
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1015
+Data        : 23/06/2026
+Objetivo    : calculo de distancia entre pontos.
+Aprendizado : <<<Usar double>>>
+-------------------------------------------------------------------------- */
 #include <stdio.h>
 #include <math.h>
 
